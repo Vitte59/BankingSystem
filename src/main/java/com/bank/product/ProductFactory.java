68ce0b8536a6b6ecdb40d3f715d19232f0249266
Vitte59 <victorcuruschin@gmail.com>
2026-09-06 23:15:58 +0300

    feat: add logging slf4j + logback

--- src/main/java/com/bank/product/ProductFactory.java
+++ src/main/java/com/bank/product/ProductFactory.java
@@ -1,16 +1,23 @@
 package com.bank.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProductFactory {
+
+    private static final Logger log =  LoggerFactory.getLogger(ProductFactory.class);
 
     public static CreditProduct createCreditProduct(String id, String name, String description, boolean isActive,
                                                     double maxSum, double minSum, String currency, LoanType loanType,
                                                     String repaymentSchedule, boolean hasCollateral) {
+        log.info("Создан кредитный продукт: id={}, name={}", id, name);
         return new CreditProduct(id, name, description, isActive, maxSum, minSum, currency,
                 loanType, repaymentSchedule, hasCollateral);
     }
 
     public static DepositProduct createDepositProduct(String id, String name, String description, boolean isActive,
                                                       double maxSum, double minSum, String currency, String earlyClosureTerms, boolean isRefillable) {
+        log.info("Создан депозитный продукт: id={}, name={}", id, name);
         return new DepositProduct(id, name, description, isActive, maxSum, minSum, currency,
                 earlyClosureTerms, isRefillable);
 
--- src/main/java/com/bank/service/ValidationService.java
+++ src/main/java/com/bank/service/ValidationService.java
@@ -5,15 +5,21 @@ import com.bank.exception.BusinessException;
 import com.bank.exception.InvalidAmountException;
 import com.bank.exception.ProductInactiveException;
 import com.bank.product.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ValidationService {
+
+    private static final Logger log = LoggerFactory.getLogger(ValidationService.class.getName());
     public void validate(Application application, Product product) throws BusinessException {
 
         if (!product.isActive()) {
+            log.error("Ошибка валидации: продукт {} неактивен", product.getId());
             throw new ProductInactiveException("Продукт должен быть активен");
         }
 
         if (application.getAmount() < product.getMinSum() || application.getAmount() > product.getMaxSum()) {
+            log.error("Ошибка валидации: amount вне допустимых пределов", product.getId());
             throw new InvalidAmountException("amount должен быть между " + product.getMinSum() + " и " +  product.getMaxSum());
         }
 
